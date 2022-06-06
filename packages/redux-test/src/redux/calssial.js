5f6b004c910e3c22be7d87f7f56b345e4b806a32
yogurt <yogurt@yogurtdeMacBook-Air.local>
2022-06-07 00:20:01 +0800

    feat: 把redux分成类和函数式两种实现

--- packages/redux-test/src/redux/calssial.js
+++ packages/redux-test/src/redux/calssial.js
@@ -1,6 +1,31 @@
 class reducer {
-  constructor(initState, action){
+  constructor(initState, action) {
     this.state = initState
     this.action = action
+  }
+
+  getState() {
+    return this.state
+  }
+
+  action(type) {
+    if (type === 'add') {
+      times++
+    } else if (type === 'sub') {
+      times--
+    }
+  }
+}
+
+class store {
+  constructor(reducer) {
+    this.reducer = reducer
+    this.listener = []
+  }
+
+  
+
+  getState() {
+    this.reducer.getState()
   }
 }
