d5b84f93bcefb94485f5e429ec2ff0175cbc4fc7
mayintao3 <mayintao3@jd.com>
2022-06-07 19:25:24 +0800

    feat: newDispatch

--- packages/redux-test/src/redux/functionalIncrease.js
+++ packages/redux-test/src/redux/functionalIncrease.js
@@ -1,7 +1,7 @@
-function reducer (state = 1, action) {
-  if(action === 'add') {
+function reducer(state = 1, action) {
+  if (action === "add") {
     return state + 1
-  } else if (action === 'sub') {
+  } else if (action === "sub") {
     return state - 1
   } else {
     return state
@@ -12,28 +12,27 @@ function createStore(reducer) {
   let state = reducer()
   let listeners = []
 
-  function subscribe (cb) {
+  function subscribe(cb) {
     listeners.push(cb)
   }
 
   function dispatch(action) {
-    
     state = reducer(state, action)
-    listeners.forEach(v => v())
+    listeners.forEach((v) => v())
     return state
   }
 
   function dispatch1(action) {
     const prevState = state
     const nextState = dispatch(action)
-    console.log('prev state', prevState)
-    console.log('action', action)
-    console.log('next state', nextState, '\n\n')
+    console.log("prev state", prevState)
+    console.log("action", action)
+    console.log("next state", nextState, "\n\n")
     return nextState
   }
 
   function dispatch2(action) {
-    if(typeof action === 'function') {
+    if (typeof action === "function") {
       action(dispatch2)
     } else {
       return dispatch1(action)
@@ -44,20 +43,44 @@ function createStore(reducer) {
     return state
   }
 
-
   return {
     subscribe,
     getState,
     dispatch,
     dispatch1,
-    dispatch2
+    dispatch2,
   }
 }
 
+function newDispatch (store, action) {
+  function log( action) {
+    const prevState = state
+    const nextState = store.dispatch(action)
+    console.log("prev state", prevState)
+    console.log("action", action)
+    console.log("next state", nextState, "\n\n")
+    return action
+  }
+  
+  function thunk (action) {
+    if(typeof action === 'function') {
+      return action(store.dispatch)
+    }
+    return action
+  }
+  store.dispatch(thunk(log(action)))
+}
 
 
+
+
+function enhancer(createStore, reducer) {
+  const store = createStore(reducer)
+}
 const store = createStore(reducer)
 
-export {
-  store
-}
+
+
+
+
+export { store }
