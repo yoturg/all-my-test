0fb84d31856d86dffe37139135d9878987e726cd
mayintao3 <mayintao3@jd.com>
2022-06-13 17:36:09 +0800

    feat: 分离children

--- packages/react-test/src/reactv1.js
+++ packages/react-test/src/reactv1.js
@@ -1,17 +1,21 @@
-import './style.css'
+import "./style.css"
 
-const app = document.querySelector('#app')
+const app = document.querySelector("#app")
 
-
-
-
-function render(tagName, props) {
-  const dom = document.createElement(tagName)
-  Object.keys(props).forEach(v => {
-    dom.setAttribute(v, props[v])
-  })
+function render({ type, props }) {
+  const dom = document.createElement(type)
+  Object.keys(props)
+    .filter((v) => v !== "children")
+    .forEach((v) => {
+      dom.setAttribute(v, props[v])
+    })
+  if (props.children && props.children.length) {
+    props.children.forEach((v) => {
+      dom.appendChild(render(v))
+    })
+  }
   return dom
 }
 
-const domNode = render('div', {class: 'red', id: 'gray'})
+const domNode = render({ type: "div", props: { class: "red", id: "gray", children: [] } })
 app.appendChild(domNode)
