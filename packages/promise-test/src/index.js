a6ab8336d1f6c321cc11851c7313de3c43353790
mayintao3 <mayintao3@jd.com>
2022-06-16 16:15:46 +0800

    完成一个非常非常简陋的promise

---
+++ packages/promise-test/src/index.js
@@ -0,0 +1,48 @@
+
+function MyPromise (fn) {
+  this.PromiseState = 'pending'
+  this.PromiseResult = undefined
+  this.thenfn = []
+  this.errorfn = []
+  function onFulfilled (res) {
+    this.PromiseState = 'fulfilled' 
+    this.PromiseResult = res
+    this.thenfn.forEach(v => {
+      v(this.PromiseResult)
+    })
+  }
+  function onRejected (res) {
+    this.PromiseState = 'rejected' 
+    this.PromiseResult = res
+    this.errorfn.forEach(v => {
+      v(this.PromiseResult)
+    })
+  }
+  fn(onFulfilled.bind(this), onRejected.bind(this))
+}
+MyPromise.prototype.then = function (fn1, fn2) {
+  if(this.PromiseState === 'pending') {
+    this.thenfn.push(fn1)
+    this.errorfn.push(fn2)
+  } else if( this.PromiseState === 'fulfilled') {
+    fn1()
+  } else {
+    fn2()
+  }
+}
+
+
+// new Promise((resolve, reject) => {
+//   setTimeout(resolve, 1000)
+// }).then(res => {
+//   console.log(111)
+// })
+
+new MyPromise((resolve, reject) => {
+  const fn = Math.random() > 0.5 ? resolve : reject
+  setTimeout(fn, 1000)
+}).then(res => {
+  console.log(111)
+}, res => {
+  console.log(222)
+})
