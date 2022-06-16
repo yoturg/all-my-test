908db27f10f34fcc0531a26c1282d0745ae6ac09
mayintao3 <mayintao3@jd.com>
2022-06-16 17:12:28 +0800

    feat: 改了一下命名

--- packages/promise-test/src/index.js
+++ packages/promise-test/src/index.js
@@ -1,41 +1,42 @@
-
-function MyPromise (fn) {
-  this.PromiseState = 'pending'
+function MyPromise(fn) {
+  this.PromiseState = "pending"
   this.PromiseResult = undefined
   this.thenfn = []
   this.errorfn = []
-  function onFulfilled (res) {
-    this.PromiseState = 'fulfilled' 
+  function resolve(res) {
+    this.PromiseState = "fulfilled"
     this.PromiseResult = res
-    this.thenfn.forEach(v => {
+    this.thenfn.forEach((v) => {
       v(this.PromiseResult)
     })
   }
-  function onRejected (res) {
-    this.PromiseState = 'rejected' 
+  function reject(res) {
+    this.PromiseState = "rejected"
     this.PromiseResult = res
-    this.errorfn.forEach(v => {
+    this.errorfn.forEach((v) => {
       v(this.PromiseResult)
     })
   }
   try {
-    fn(onFulfilled.bind(this), onRejected.bind(this))
-  } catch(e) {
+    fn(resolve.bind(this), reject.bind(this))
+  } catch (e) {
     onRejected(e)
   }
 }
-MyPromise.prototype.then = function (fn1, fn2) {
-  if(this.PromiseState === 'pending') {
-    this.thenfn.push(fn1)
-    this.errorfn.push(fn2)
-  } else if( this.PromiseState === 'fulfilled') {
-    fn1()
+MyPromise.prototype.then = function (onFulfilled, onRejected) {
+  onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : (res) => {return res}
+  onRejected = typeof onRejected === 'function' ? onRejected : (e) => {throw e}
+  
+  if (this.PromiseState === "pending") {
+    this.thenfn.push(onFulfilled)
+    this.errorfn.push(onRejected)
+  } else if (this.PromiseState === "fulfilled") {
+    onFulfilled()
   } else {
-    fn2()
+    onRejected()
   }
   return this
 }
-
 
 // new Promise((resolve, reject) => {
 //   setTimeout(resolve, 1000)
@@ -46,12 +47,20 @@ MyPromise.prototype.then = function (fn1, fn2) {
 new MyPromise((resolve, reject) => {
   const fn = Math.random() > 0.5 ? resolve : reject
   setTimeout(fn, 1000)
-}).then(res => {
-  console.log(111)
-}, res => {
-  console.log(222)
-}).then(res => {
-  console.log(333)
-}, e => {
-  console.log(444)
 })
+  .then(
+    (res) => {
+      console.log(111)
+    },
+    (res) => {
+      console.log(222)
+    }
+  )
+  .then(
+    (res) => {
+      console.log(333)
+    },
+    (e) => {
+      console.log(444)
+    }
+  )
