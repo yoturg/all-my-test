662dc510e3c7e4c0f80e9efb284d87a4498d8f8b
yogurt <434354564@qq.com>
2022-07-27 17:07:16 +0800

    feat: 完成了函数定义产生的依赖的收集

--- packages/ast-test/src/dependencyCollection.js
+++ packages/ast-test/src/dependencyCollection.js
@@ -14,58 +14,56 @@ function collectionWithFnName(ast, fileName, names) {
   const dir = nodePath.parse(fileName).dir
   const res = {}
   traverse(ast, {
-    FunctionDeclaration(path) {
-      if (names.includes(path.node.id.name)) {
-        const callList = {}
-        path.traverse({
-          CallExpression(p) {
-            const fnName = p.node.callee.name
-            try{
-                const binding = path.scope.getBinding(fnName)
-
-                const sourcePath = t.isImportSpecifier(binding?.path) ? 
-                nodePath.join(dir, binding.path.parentPath.node.source.value) :
-                fileName
-                callList[sourcePath] = callList[sourcePath] || []
-                callList[sourcePath].push(fnName)
-            } catch(e) {
-              console.log(e)
-              console.log('++++++++++++++++++++')
-              console.log('names', names)
-              console.log('fileName', fileName)
-              console.log('name', fnName)
-            }
-          },
-        })
-        res[path.node.id.name] = callList
+    ExportNamedDeclaration(path){
+      const { node } = path
+      if(t.isFunctionDeclaration(node.declaration)) {
+        const fnPath = path.get('declaration')
+        if (names.includes(fnPath.node.id.name)) {
+          const callList = {}
+          fnPath.traverse({
+            CallExpression(p) {
+              const fnName = p.node.callee.name
+              try {
+                const binding = fnPath.scope.getBinding(fnName)
+                if (binding && binding.path) {
+                  const sourcePath = t.isImportSpecifier(binding?.path) ? nodePath.join(dir, binding.path.parentPath.node.source.value) : fileName
+                  callList[sourcePath] = callList[sourcePath] || []
+                  if (!callList[sourcePath].includes(fnName)) {
+                    callList[sourcePath].push(fnName)
+                  }
+                }
+              } catch (e) {
+                console.log('names', names)
+                console.log('fileName', fileName)
+                console.log('name', fnName)
+              }
+            },
+          })
+          res[fnPath.node.id.name] = callList
+        }
       }
     },
+  
   })
-  return res 
+  return res
 }
 
 function tidy(fileName, fnCache, callList) {
-  // console.log('++++++++++++++++')
-  // console.log('fileName', fileName)
-  // console.log('fnCache', fnCache) 
-  // console.log('callList', callList)
- 
-
-  Object.keys(callList).forEach(fnName => {
+  Object.keys(callList).forEach((fnName) => {
     const item = callList[fnName]
     const file = fnCache[fileName] || fnCache[fileName.replace(/\.js$/, '')]
-    if(file && file.hasOwnProperty(fnName)) {
+    if (file && file.hasOwnProperty(fnName)) {
       file[fnName] = item
     }
-    Object.keys(item).forEach((v) => {
-      
-      fnCache[v] = fnCache[v] || {}
-      item[v].forEach((val) => {
-        fnCache[v][val] = fnCache[v][val] || null
+    Object.keys(item).forEach((filePath) => {
+      fnCache[filePath] = fnCache[filePath] || {}
+      item[filePath].forEach((fn) => {
+        fnCache[filePath][fn] = fnCache[filePath][fn] || null
       })
     })
   })
 }
+
 
 ;(async () => {
   try {
@@ -74,33 +72,40 @@ function tidy(fileName, fnCache, callList) {
 
     const fnCache = {}
 
-    fnCache[fileName] = { 'updateContainer': null }
-  
-    let isNew = 3
+    fnCache[fileName] = { updateContainer: null }
+
+    let isNew = 7
     while (isNew) {
-      isNew-- 
+      isNew--
       for (let file in fnCache) {
-        const list = Object.keys(fnCache[file]).filter(v => fnCache[file][v] === null)
-        console.log(list)
-        if(list.length) {
+        const list = Object.keys(fnCache[file]).filter((v) => fnCache[file][v] === null)
+        if (list.length) {
+          console.log(list)
           // isNew = true
           let str = null
-          try{
-            str = await fs.readFileSync(file)
-          } catch(e){
-            file += '.js'
-            str = await fs.readFileSync(file)
+          let usedFile = file
+          try {
+            str = await fs.readFileSync(usedFile)
+          } catch (e) {
+            usedFile += '.js'
+            str = await fs.readFileSync(usedFile)
           }
+
           const ast = babelParser.parse(str.toString(), {
             sourceType: 'module',
           })
           const res = collectionWithFnName(ast, file, list)
+          if(file === '/Users/yogurt/workspace/test/react-lego/step4/react-reconciler-new/src/ReactFiberWorkLoop.new.js') {
 
+            console.log('++++++++++++++++++++++++++')
+            console.log('-----', JSON.stringify(res))
+            console.log('-----', JSON.stringify(fnCache))
+          }
           tidy(file, fnCache, res)
+
         }
       }
     }
-    
 
     // tidy(fnCache, callList)
 
@@ -110,3 +115,14 @@ function tidy(fileName, fnCache, callList) {
     console.log(e)
   }
 })()
+
+// ;(async function test() {
+//   const entrance = nodePath.resolve('/Users/yogurt/workspace/test/react-lego/step4/react-reconciler-new/src/')
+//   const fileName = nodePath.resolve(entrance, 'ReactFiberWorkLoop.new.js')
+//   const str = await fs.readFileSync(fileName)
+//   const ast = babelParser.parse(str.toString(), {
+//     sourceType: 'module',
+//   })
+//   const res = collectionWithFnName(ast, fileName, [ "isUnsafeClassRenderPhaseUpdate", "throwIfInfiniteUpdateLoopDetected"])
+//   console.log(res)
+// })()
