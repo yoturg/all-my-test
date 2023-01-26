1e9158c2fe99c47279ed32960f0ea363f4201996
yoturg <434354564@qq.com>
2023-01-26 21:49:04 +0800

    style: 重构

---
+++ packages/shader/src/cloud/index.js
@@ -0,0 +1,181 @@
+import Stats from 'stats.js'
+import '../util'
+import fs from './fragmentShader.glsl'
+import vs from './vertexShader.glsl'
+
+export default function (canvas) {
+  const gl = canvas.getContext('webgl')
+  if (!gl) {
+    return
+  }
+
+  function shaderfn(gl_FragCoord, iResolution, iTime) {
+    const tgtDst = 3.5
+
+    const p = gl_FragCoord.xy.sub(iResolution.xy.mult(0.5)).divide(min(iResolution.y, iResolution.x))
+
+    const target = normalize(vec3(0, 0, -tgtDst))
+    // const p = (gl_FragCoord.xy - 0.5 * iResolution.xy) / min(iResolution.y, iResolution.x)
+
+    const time = iTime * 3
+    const ro = vec3(0, 0, time)
+    return p
+  }
+
+  function moveEventHandler(e) {
+    let x = 0,
+      y = 0
+    if (e.type === 'mousemove') {
+      x = e.x
+      y = e.y
+    } else {
+      x = e.changedTouches[0].pageX
+      y = e.changedTouches[0].pageY
+    }
+
+    const pageWidth = innerWidth
+    const pageHeight = innerHeight
+
+    const pointWidth = point.offsetWidth
+    const pointHeight = point.offsetHeight
+
+    let top = y
+    let left = x
+    if (x + pointWidth >= pageWidth - 10) {
+      left -= pointWidth
+    }
+    if (y + pointHeight >= pageHeight - 10) {
+      top -= pointHeight
+    }
+
+    point.style.top = top + 'px'
+    point.style.left = left + 'px'
+
+    const res = shaderfn(vec2(x, y), vec2(pageWidth, pageHeight), iTime)
+    point.innerText = `${res.x}, ${res.y}`
+    e.stopPropagation()
+  }
+
+  function endEventHandler(e) {
+    point.style.top = 'unset'
+    point.style.left = 'unset'
+  }
+
+  canvas.addEventListener('mousemove', moveEventHandler)
+  canvas.addEventListener('touchmove', moveEventHandler)
+
+  canvas.addEventListener('mouseup', endEventHandler)
+  canvas.addEventListener('touchend', endEventHandler)
+
+  // setup GLSL program
+  const program = webglUtils.createProgramFromSources(gl, [vs, fs])
+
+  // look up where the vertex data needs to go.
+  const positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
+
+  // look up uniform locations
+  const resolutionLocation = gl.getUniformLocation(program, 'iResolution')
+  const mouseLocation = gl.getUniformLocation(program, 'iMouse')
+  const timeLocation = gl.getUniformLocation(program, 'iTime')
+
+  // Create a buffer to put three 2d clip space points in
+  const positionBuffer = gl.createBuffer()
+
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+
+  // fill it with a 2 triangles that cover clipspace
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      -1,
+      -1, // first triangle
+      1,
+      -1,
+      -1,
+      1,
+      -1,
+      1, // second triangle
+      1,
+      -1,
+      1,
+      1,
+    ]),
+    gl.STATIC_DRAW
+  )
+
+  let mouseX = 0
+  let mouseY = 0
+
+  const stats = new Stats()
+  stats.showPanel(0)
+  // document.body.appendChild(stats.dom)
+
+  let requestId
+  function requestFrame() {
+    if (!requestId) {
+      stats.begin()
+      requestId = requestAnimationFrame(render)
+      stats.end()
+    }
+  }
+  function cancelFrame() {
+    if (requestId) {
+      cancelAnimationFrame(requestId)
+      requestId = undefined
+    }
+  }
+
+  let then = 0
+  let time = 0
+  function render(now) {
+    requestId = undefined
+    now *= 0.001 // convert to seconds
+    const elapsedTime = Math.min(now - then, 0.1)
+    time += elapsedTime
+    then = now
+
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas)
+
+    // Tell WebGL how to convert from clip space to pixels
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(program)
+
+    // Turn on the attribute
+    gl.enableVertexAttribArray(positionAttributeLocation)
+
+    // Bind the position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+
+    // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    gl.vertexAttribPointer(
+      positionAttributeLocation,
+      2, // 2 components per iteration
+      gl.FLOAT, // the data is 32bit floats
+      false, // don't normalize the data
+      0, // 0 = move forward size * sizeof(type) each iteration to get the next position
+      0 // start at the beginning of the buffer
+    )
+
+    gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height)
+    gl.uniform2f(mouseLocation, mouseX, mouseY)
+    window.iTime = time
+
+    gl.uniform1f(timeLocation, time)
+
+    gl.drawArrays(
+      gl.TRIANGLES,
+      0, // offset
+      6 // num vertices to process
+    )
+
+    requestFrame()
+  }
+
+  requestFrame()
+  requestAnimationFrame(cancelFrame)
+}
+
+
---
+++ packages/shader/src/util.js
@@ -0,0 +1,216 @@
+const radians = function (deg) {
+  return (Math.PI / 180) * deg
+}
+
+const degrees = function (rad) {
+  return (180 / Math.PI) * deg
+}
+
+const clamp = function (x, min, max) {
+  if (min > max) return
+  return Math.min(Math.max(x, min), max)
+}
+
+const smoothstep = function (edge0, edge1, x) {
+  if (edge0 >= edge1) return
+  if (x <= edge0) return 0
+  if (x >= edge1) return 1
+
+  const t = clamp((x - edge0) / (edge1 - edge0), 0, 1)
+  return t * t * (3 - 2 * t)
+}
+
+const normalize = function (vec) {
+  const len = Math.sqrt(
+    vec.reduce((acc, cur) => {
+      return acc + cur * cur
+    }, 0)
+  )
+
+  return vec.map((v) => v / len)
+}
+
+const dot = function (x, y) {
+  return x.reduce((acc, cur, idx) => {
+    return acc + cur * y[idx]
+  }, 0)
+}
+
+const cross = function (x, y) {
+  return [x[1] * y[2] - y[1] * x[2], x[2] * y[0] - y[2] * x[0], x[0] * y[1] - y[0] * x[1]]
+}
+
+function permutation(str, times) {
+  const list = Array.from(str)
+  if (times === 1) return list
+  const nextStrMap = permutation(str, times - 1)
+  return list.reduce((acc, cur) => {
+    return acc.concat(
+      cur,
+      nextStrMap.map((newStr) => `${cur}${newStr}`)
+    )
+  }, [])
+}
+
+const keyList = ['xyzw', 'rgba', 'stpq']
+
+function createVec(dim) {
+  const keyMap = permutation('0123'.slice(0, dim), dim)
+  return function (...args) {
+    let list = new Array(dim)
+    if (args.length === 1 && typeof args[0] === 'number') {
+      list.fill(args[0])
+    } else {
+      list = Array.prototype.concat.apply([], args)
+      if (list.length !== dim) {
+        throw new Error('not enough data provided for construction')
+      }
+    }
+
+    keyMap.forEach((numStr) => {
+      const numList = Array.from(numStr)
+
+      const keys = keyList.map((originKey) => {
+        return numList.map((idx) => originKey.charAt(idx)).join('')
+      })
+      keys.forEach((key) => {
+        Object.defineProperty(list, key, {
+          enumerable: false,
+          set(l) {
+            l = [].concat(l)
+            if (
+              numList.some((val, ind, arr) => {
+                return arr.lastIndexOf(val) !== ind
+              })
+            ) {
+              throw new Error('used twice')
+            }
+            if (l.length !== numList.length) {
+              throw new Error(`mismatch between vec${numList.length} and vec${l.length}`)
+            }
+            numList.forEach((num, index) => {
+              list[num] = l[index]
+            })
+          },
+          get() {
+            if (numList.length === 1) {
+              return list[numList[0]]
+            }
+            const vec = createVec(numList.length)
+            return vec(numList.map((ind) => list[ind]))
+          },
+        })
+      })
+    })
+    list.add = function (num) {
+      const result = createVec(dim)(this)
+      if (typeof num === 'number') {
+        result.forEach((v, i) => {
+          result[i] += num
+        })
+      } else if (num.type === 'vec' && num.length === result.length) {
+        result.forEach((v, i) => {
+          result[i] += num[i]
+        })
+      }
+
+      return result
+    }
+    list.sub = function (num) {
+      if (typeof num === 'number') {
+        return list.add(-1 * num)
+      } else if (num.type === 'vec') {
+        return list.add(num.mult(-1))
+      }
+    }
+    list.mult = function (num) {
+      const result = createVec(dim)(this)
+      if (typeof num === 'number') {
+        result.forEach((v, i) => {
+          result[i] *= num
+        })
+      } else if (num.type === 'mat' && num.length === result.length) {
+        this.forEach((v, i) => {
+          result[i] = dot(this, num[i])
+        })
+      }
+      return result
+    }
+
+    list.divide = function (num) {
+      if (typeof num === 'number') {
+        return list.mult(1 / num)
+      }
+    }
+
+    list.type = 'vec'
+    list.dim = dim
+    return list
+  }
+}
+
+function createMat(dim) {
+  const vec = createVec(dim)
+  return function (...args) {
+    let list = new Array(dim).fill(null)
+    if (args.length === 1) {
+      list = list.map((_, i) => {
+        const vector = vec(0)
+        vector[i] = args[0]
+        return vector
+      })
+    } else if (args.length === dim * dim) {
+      list = list.map((_, i) => {
+        const vecList = new Array(dim).fill(null).map((__, inx) => args[inx + i * dim])
+        return vec(vecList)
+      })
+    } else {
+      throw new Error('not enough data provided for construction')
+    }
+    list.mult = function (args) {
+      if (args.type === 'vec' && args.dim === dim) {
+        const m = this
+        const v = args
+
+        return v
+          .map((num, ind) => m[ind].mult(num))
+          .reduce((acc, cur) => {
+            return vec(acc.add(cur))
+          }, vec(0))
+      } else if (args.type === 'mat' && args.dim === dim) {
+        return args.map((n) => this.mult(n))
+      } else {
+        throw new Error('错了，或者这个情况我还没写')
+      }
+    }
+    list.type = 'mat'
+    list.dim = dim
+    return list
+  }
+}
+for (let i = 2; i <= 4; i++) {
+  window[`vec${i}`] = createVec(i)
+  window[`mat${i}`] = createMat(i)
+}
+
+window.smoothstep = smoothstep
+window.cross = cross
+window.dot = dot
+window.normalize = normalize
+window.clamp = clamp
+
+window.min = Math.min
+window.max = Math.max
+window.exp = Math.exp
+window.sin = Math.sin
+window.cos = Math.cos
+window.tan = Math.tan
+window.asin = Math.asin
+window.acos = Math.acos
+window.atan = function (...args) {
+  if (args.length === 1) {
+    Math.atan(...args)
+  } else {
+    Math.atan2(...args.slice(0, 2))
+  }
+}
