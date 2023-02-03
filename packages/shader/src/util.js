5f6cc712ec2f9f8329f25b1210fbc9ce448b266c
yogurt <434354564@qq.com>
2023-02-03 17:40:33 +0800

    feat

--- packages/shader/src/util.js
+++ packages/shader/src/util.js
@@ -256,32 +256,3 @@ window.test = (s) => {
 }
 
 console.log(test())
-
-window.dataURLtoBlob = function dataURLtoBlob(dataurl) {
-  const arr = dataurl.split(',')
-  const mime = arr[0].match(/:(.*?);/)[1]
-  const bstr = atob(arr[1])
-  let n = bstr.length
-  const u8arr = new Uint8Array(n)
-  while (n--) {
-    u8arr[n] = bstr.charCodeAt(n)
-  }
-  return new Blob([u8arr], { type: mime })
-}
-
-// 将blob转换为file
-window.blobToFile = function blobToFile(theBlob, fileName) {
-  console.log('二进制转对象')
-  theBlob.lastModifiedDate = new Date()
-  theBlob.name = fileName
-  return theBlob
-}
-
-
-window.downLoad = function (blob) {
-  const link = document.createElement('a');
-  const objurl = URL.createObjectURL(blob)
-  link.download = 'aa.png';
-  link.href = objurl
-  link.click()  
-}
